4d574151ad8b7a7a507095631f370db2bd5b2e12
Luciano Melo <luciano.mrbuch@gmail.com>
2025-06-05 21:58:25 -0300

    SellerService e FindAll Implementado

--- WebSalesMVC/Controllers/SellersController.cs
+++ WebSalesMVC/Controllers/SellersController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebSalesMVC.Services;
 
 namespace WebSalesMVC.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            var listFindAll = _sellerService.FindAll();
+            return View(listFindAll);
         }
+
+         
     }
 }
--- WebSalesMVC/Services/SellerService.cs
+++ WebSalesMVC/Services/SellerService.cs
@@ -1,9 +1,23 @@
-﻿using WebSalesMVC.Data;
+﻿using System.Linq;
+using WebSalesMVC.Data;
+using WebSalesMVC.Models;
 
 namespace WebSalesMVC.Services
 {
     public class SellerService
     {
         private readonly WebSalesMVCContext _context;
+
+        public SellerService(WebSalesMVCContext context)
+        {
+            _context = context;
+
+        }
+
+        public System.Collections.Generic.List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+
+        }
     }
 }
